e906e6435314c1c82e28072611049048db854b4b
jim <jim.miracle.wang@hotmail.com>
2017-03-22 23:10:50 +0800

    河南国税 验证码测试成功

--- WebDemoAsp/Controllers/HomeController.cs
+++ WebDemoAsp/Controllers/HomeController.cs
@@ -19,19 +19,19 @@ namespace WebDemoAsp.Controllers
     public class HomeController : Controller
     {
         private static string CookieStr;
-
+        private static string CurrentPath = HttpRuntime.AppDomainAppPath.ToString();
         public ActionResult Index()
         {
             //var source= @"{"message":"鎮ㄦ墍鏌ヨ鐨勫彂绁ㄤ俊鎭笌鎶ョ◣璁板綍涓嶄竴鑷达紒","lsh":"TypeJ2017032010101210.104.120.106","flag":true,"queryCount":4,"options":[{"flag":0,"name":"鍙戠エ浠ｇ爜","inputValue":"012001600111"},{"flag":0,"name":"鍙戠エ鍙风爜","inputValue":"10645196"},{"flag":0,"name":"寮€绁ㄦ棩鏈 ?,"inputValue":"2017-03-09"},{"flag":2,"name":"璐揣鏂圭◣鍙?,"inputValue":""},{"flag":0,"name":"閿€璐ф柟绋庡彿","inputValue":"911201163409833307"},{"flag":1,"name":"閿€璐ф柟鍚嶇О","inputValue":"忙禄麓忙禄麓氓\u0087潞猫隆\u008C莽搂\u0091忙\u008A\u0080忙\u009C\u0089茅\u0099\u0090氓\u0085卢氓\u008F赂"},{"flag":0,"name":"閲戦","inputValue":"36.7"},{"flag":0,"name":"绋庨","inputValue":"1.1"},{"flag":0,"name":"浠风◣鍚堣","inputValue":"37.80"}]}";
 
-            GetTianjinMethod();
+            GetHenanMethod();
             return View();
         }
 
         [HttpPost]
         public ActionResult Index(string model)
         {
-            var result = GetTianjinPostMethod(Request.Form.Get("vcode"));
+            var result = GetHenanPostMethod(Request.Form.Get("vcode"));
             if (result.Contains("请输入正确的验证码"))
             {
                 return RedirectToAction("Index");
@@ -46,7 +46,7 @@ namespace WebDemoAsp.Controllers
 
             //string CookieStr = GetMethod();
             GetHenanMethod();
-            GetHenanPostMethod();
+            //GetHenanPostMethod();
             // POST
             //var requestModel = new Sichuan() { fpdm = "151001665018", fphm = "01444200", type = "0" };
             //var s = JsonSerializer.Create();
@@ -148,54 +148,20 @@ fpcy.type=0"));
 
         private static string GetHenanMethod()
         {
-            // GET
             WebClient wbx = new WebClient();
-            //HttpClient client = new HttpClient();
-            //wbx.BaseAddress = StateTaxationConstants.Sichuan;
-            //wbx.BaseAddress = StateTaxationConstants.Sichuan;Accept:*/*
             wbx.Headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
             wbx.Headers.Add("Upgrade-Insecure-Requests", "1");
-
-            //wbx.Headers.Add("Origin", "http://bsfwt.12366.ha.cn");
             wbx.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36");
             wbx.Headers.Add("Accept-Encoding", "gzip, deflate");
             wbx.Headers.Add("Accept-Language", "zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4");
             Encoding enc = Encoding.GetEncoding("UTF-8");
-            wbx.DownloadFile(StateTaxationConstants.HenanGet, @"D:/codeHenan.png");
-
+            wbx.DownloadFile(StateTaxationConstants.HenanGet, $@"{CurrentPath}/Vcode/Vcode.png");
             CookieStr = wbx.ResponseHeaders["Set-Cookie"].ToString();
-            //File storeFile = new File("c:/2008sohu.gif");
-            //FileOutputStream output = new FileOutputStream(storeFile);
-            ////得到网络资源的字节数组,并写入文件    
-            //output.write(getMethod.getResponseBody());
-            //output.close();
-            //InputStreamReader is = new InputStreamReader(System.in);
-            //BufferedReader br = new BufferedReader(is);
-            //String ValidCode = "";
-            //try
-            //{
-            //    ValidCode = br.readLine();
-            //    br.close();
-            //is.close();
-            //}
-            //catch (Exception e)
-            //{
-            //    e.printStackTrace();
-            //}
             return CookieStr;
-
-
-            //string re = enc.GetString(pageData);
-            //var CookieStr = wbx.ResponseHeaders["Set-Cookie"].ToString();
-            //return CookieStr;
         }
-        private static string GetHenanPostMethod()
+        private static string GetHenanPostMethod(string vcode)
         {
-            // GET
             WebClient wbx = new WebClient();
-            //HttpClient client = new HttpClient();
-            //wbx.BaseAddress = StateTaxationConstants.Sichuan;
-            //wbx.BaseAddress = StateTaxationConstants.Sichuan;Accept:*/*
             wbx.Headers.Add("Accept", "application/json, text/javascript, */*");
             wbx.Headers.Add("Origin", "http://bsfwt.12366.ha.cn");
             wbx.Headers.Add("X-Requested-With", "XMLHttpRequest");
@@ -206,41 +172,33 @@ fpcy.type=0"));
             wbx.Headers.Add("Accept-Encoding", "gzip, deflate");
             wbx.Headers.Add("Accept-Language", "zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4");
             wbx.Headers.Add("Cookie", CookieStr);
+            wbx.Encoding = Encoding.UTF8;
+
             Encoding enc = Encoding.GetEncoding("UTF-8");
-            string vCode = string.Empty;
-            //wbx.DownloadFile(StateTaxationConstants.Henan2, @"D:/codeHenan.png");//Byte[] pageData = 
-            Byte[] pageReponseData = wbx.UploadData(StateTaxationConstants.HenanPost, "POST", Encoding.UTF8.GetBytes($@"ACTION=getFpzw&
-FPDM=141001620043&
-FPHM=09767750&
-NSRSBH=92410300MA3XG28Q9U&
-rcode={vCode}"));
-            Encoding enc2 = Encoding.GetEncoding("UTF-8");
-            string reponse = enc2.GetString(pageReponseData);
-            //CookieStr = wbx.ResponseHeaders["Set-Cookie"].ToString();
-            //File storeFile = new File("c:/2008sohu.gif");
-            //FileOutputStream output = new FileOutputStream(storeFile);
-            ////得到网络资源的字节数组,并写入文件    
-            //output.write(getMethod.getResponseBody());
-            //output.close();
-            //InputStreamReader is = new InputStreamReader(System.in);
-            //BufferedReader br = new BufferedReader(is);
-            //String ValidCode = "";
-            //try
-            //{
-            //    ValidCode = br.readLine();
-            //    br.close();
-            //is.close();
-            //}
-            //catch (Exception e)
-            //{
-            //    e.printStackTrace();
-            //}
-            return CookieStr;
+            string vCode = vcode;
+            StringBuilder sb = new StringBuilder();
+            sb.Append(HttpUtility.UrlEncode("ACTION"));
+            sb.Append("=");
+            sb.Append(HttpUtility.UrlEncode("getFpzw"));
+            sb.Append("&");
+            sb.Append(HttpUtility.UrlEncode("FPDM"));
+            sb.Append("=");
+            sb.Append(HttpUtility.UrlEncode("141001620043"));
+            sb.Append("&");
+            sb.Append(HttpUtility.UrlEncode("FPHM"));
+            sb.Append("=");
+            sb.Append(HttpUtility.UrlEncode("09767750"));
+            sb.Append("&");
+            sb.Append(HttpUtility.UrlEncode("NSRSBH"));
+            sb.Append("=");
+            sb.Append(HttpUtility.UrlEncode("92410300MA3XG28Q9U"));
+            sb.Append("&");
+            sb.Append(HttpUtility.UrlEncode("rcode"));
+            sb.Append("=");
+            sb.Append(HttpUtility.UrlEncode(vCode));
+            var result = wbx.UploadString(StateTaxationConstants.HenanPost, "POST", sb.ToString());
 
-
-            //string re = enc.GetString(pageData);
-            //var CookieStr = wbx.ResponseHeaders["Set-Cookie"].ToString();
-            //return CookieStr;
+            return result;
         }
 
         private static string GetTianjinMethod()
